c8d22b744086185088f901e3e6765275cb54b798
Marco Arduini <mar.arduini@gmail.com>
2017-01-11 12:50:33 +0100

    Implement the redux side

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store';
+import { addOrange, addApple, clearFruit } from './actions';
+
+window.store = store; // just for testing
+window.addOrange = addOrange;
+window.clearFruit = clearFruit;
 
 const App = () => (
   <div>
